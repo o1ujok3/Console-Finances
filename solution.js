3b00f44f92c7f922e6f4cd30c6432ddadd0292a4
Olujoke Fakoya <113132389+o1ujok3@users.noreply.github.com>
2023-02-06 16:54:08 +0000

    codes to questions added to solution.js file

--- solution.js
+++ solution.js
@@ -1,53 +1,28 @@
-// Finding the length of the initial array
-console.log('Total Months:  + finances.length);
+//Variables
+var totalMonths = 0;
+var sumOfProfits = 0;
+var diffPerMonth = finances;
+var diff = 0;
+var sumOfDiff = 0;
+var greatestIncrease = [0, 0, 0];
+var greatestDecrease = [0, 0, 0];
 
-// I created a new array called sales to hold only the numerical values
-for (let i = 0; i < finances.length; i++) {
-  console.log(finances[i][1]);
+//The total number of months included in the dataset.
+var totalMonths = finances.length;
+console.log("total months:" + totalMonths);
+
+for (i = 0; i < finances.length; i++) {
+  //Net profit/losses over entire period
+  sumOfProfits += finances[i][1];
+  //To calculate the individual changes per month
+  if (i > 0) {
+    diff = diffPerMonth[i][1] - diffPerMonth[i - 1][1];
+    diffPerMonth[i].push(diff);
+    sumOfDiff += diffPerMonth[i][2];
+    //To find the greatest increase in profit
+    greatestIncrease = diffPerMonth[i];
+    //To find the greatest decrease in profit
+  } else if (greatestDecrease[2] > diffPerMonth[i][2]) {
+    greatestDecrease = diffPerMonth[i];
+  }
 }
-
-let sales = [];
-for (let i = 0; i < finances.length; i++) {
-  sales.push(finances[i][1]);
-  console.log(sales);
-}
-
-//sum of the values of profit/loss
-let sum = 0;
-
-for (let i = 0; i < sales.length; i++) {
-  sum += sales[i];
-
-  console.log("Total: " + sum);
-}
-
-// finding the difference in Value in the elements and adding them to a new array
-let diff = [];
-for (let i = 0; i < sales.length; i++) {
-  diff.push(sales[i] - sales[i - 1]);
-
-  console.log(diff);
-}
-
-// since the code for the difference will not have value (NaN) for the index[0] because index[0] cannot be subtracted from a previous value, it is manually appended to the new [diff] array.
-diff[0] = sales[0];
-console.log(diff);
-
-//Total of the difference in each month is then calculated
-let total = 0;
-for (let i = 0; i < diff.length; i++) {
-  total += diff[i];
-  console.log(total);
-}
-
-//From the total, average is determined by using length of the array
-let avg = total / diff.length;
-console.log("Average Change: " + avg);
-
-// The maximum and minimum value in the difference array (not determined, yet).
-
-let max = Math.max(diff);
-console.log("Greatest Increase in profit: " + max);
-
-let min = Math.min(diff);
-console.log("Greatest Dncrease in profit: " + min);
